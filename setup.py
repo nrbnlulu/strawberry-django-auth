d540883c5ad1d02042c092229ebeea399525a7e3
nir <nir@tzevet5.co.il>
2022-04-18 14:54:33 +0300

    tests with gh and tox

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     ),
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
-        "Django>=3.3",
+        "Django>=3.2.13",
         "strawberry-django-jwt>=0.2.0",
         "strawberry-graphql-django>=0.2.5",
         "strawberry-graphql>=0.104.3",
