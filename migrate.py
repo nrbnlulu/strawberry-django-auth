8d4ff2cd08f5cf63298ac8f78804491e1e2b1160
nir <nir@tzevet5.co.il>
2022-04-18 21:57:20 +0300

    tests with gh and tox

--- migrate.py
+++ migrate.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "migrations_settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings_b")
     from django.core.management import execute_from_command_line
 
-    args = sys.argv + ["makemigrations", "gqlauth"]
+    args = sys.argv + ["migrate"]
     execute_from_command_line(args)
