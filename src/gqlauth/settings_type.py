9d089cc054a76baf502d818a078c96a41bb5a1b3
nir <nrbnlulu@gmail.com>
2022-05-29 13:55:50 +0300

    bump version

--- src/gqlauth/settings_type.py
+++ src/gqlauth/settings_type.py
@@ -30,10 +30,14 @@ class GqlAuthSettings:
     # mutation fields options
     LOGIN_OPTIONAL_FIELDS: list = field(default_factory=lambda: [])
     LOGIN_REQUIRE_CAPTCHA: bool = True
-    LOGIN_REQUIRED_FIELDS: dict | list = field(default_factory=lambda: ["username", "password"])
+    LOGIN_REQUIRED_FIELDS: dict | list = field(
+        default_factory=lambda: ["username", "password"]
+    )
     # required fields on register, plus password1 and password2,
     # can be a dict like UPDATE_MUTATION_fieldS setting
-    REGISTER_MUTATION_FIELDS: dict | list = field(default_factory=lambda: ["email", "username"])
+    REGISTER_MUTATION_FIELDS: dict | list = field(
+        default_factory=lambda: ["email", "username"]
+    )
     REGISTER_MUTATION_FIELDS_OPTIONAL: list = field(default_factory=lambda: [])
     REGISTER_REQUIRE_CAPTCHA: bool = True
     # captcha stuff
@@ -80,8 +84,12 @@ class GqlAuthSettings:
     EMAIL_TEMPLATE_PASSWORD_RESET: str = "email/password_reset_email.html"
     EMAIL_TEMPLATE_VARIABLES: dict = field(default_factory=lambda: {})
     # query stuff
-    USER_NODE_EXCLUDE_FIELDS: dict | list = field(default_factory=lambda: ["password", "is_superuser"])
-    USER_NODE_FILTER_FIELDS: dict = field(default_factory=lambda: _USER_NODE_FILTER_fieldS)
+    USER_NODE_EXCLUDE_FIELDS: dict | list = field(
+        default_factory=lambda: ["password", "is_superuser"]
+    )
+    USER_NODE_FILTER_FIELDS: dict = field(
+        default_factory=lambda: _USER_NODE_FILTER_fieldS
+    )
     # others
     ## turn is_active to False instead
     ALLOW_DELETE_ACCOUNT: bool = False
