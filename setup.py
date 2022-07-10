a7738e6f473b7cf63ccd340c3385d7d2dabf6e41
Nir <88795475+nrbnlulu@users.noreply.github.com>
2022-07-10 12:28:59 +0300

    Release: 0.3.3.1 - dependencies update.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# adopted from strawberry to enable dependants in GitHub
+# Don't use this
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(name="strawberry-django-auth")
