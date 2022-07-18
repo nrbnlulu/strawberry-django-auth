eb54ccbd9295ee56d1279a6368097266f7f24d69
Nir <88795475+nrbnlulu@users.noreply.github.com>
2022-07-18 19:04:59 +0300

    migrate to async - Fixed async test that work: captcha, archive account, test_delete_account.py, test_login.py, test_password_change.py, test_password_reset.py, test_password_set.py, test_query.py, test_refresh_token.py, test_register.py, test_remove_secondary_email.py, test_revoke_token.py, test_send_password_reset_email.py, test_swap_emails.py, test_update_account.py

--- tests/test_swap_emails.py
+++ tests/test_swap_emails.py
@@ -37,7 +37,8 @@ class SwapEmailsCaseMixin:
         assert user_obj.status.secondary_email == user.email
 
     def test_swap_emails_without_secondary_email(self, db_verified_user_status):
-        executed = self.make_request(query=self.make_query(db_verified_user_status.user), user_status=db_verified_user_status)
+        executed = self.make_request(query=self.make_query(db_verified_user_status.user),
+                                     user_status=db_verified_user_status)
         assert not executed["success"]
         assert executed["errors"]["nonFieldErrors"] == Messages.SECONDARY_EMAIL_REQUIRED
 
--- tests/test_update_account.py
+++ tests/test_update_account.py
@@ -6,159 +6,7 @@ from .testCases import ArgTestCase, RelayTestCase
 
 
 class UpdateAccountTestCaseMixin:
-    def setUp(self):
-        unverified_user = self.register_user(
-            email="foo@email.com", username="foo", verified=False, first_name="foo"
-        )
-        self.user2 = self.register_user(
-            email="bar@email.com", username="bar", verified=True, first_name="bar"
-        )
-        self.user3 = self.register_user(
-            email="gaa@email.com", username="gaa", verified=True, first_name="gaa"
-        )
-
-    def test_update_account_unauthenticated(self):
-        executed = self.make_request(self.make_query())
-        assert not executed["success"]
-        assert user_status.user.password != user.password == Messages.UNAUTHENTICATED
-
-    def test_update_account_not_verified(self):
-        variables = {"user": unverified_user}
-        executed = self.make_request(self.make_query(), variables)
-        assert not executed["success"]
-        assert user_status.user.password != user.password == Messages.NOT_VERIFIED
-
-    def test_update_account(self):
-        variables = {"user": self.user2}
-        executed = self.make_request(self.make_query(), variables)
-        assert executed["success"]
-        assert not executed["errors"]
-        self.user2.refresh_from_db()
-        self.assertEqual(self.user2.first_name, "firstname")
-
-    def test_invalid_form(self):
-        super_long_string = (
-            "longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1-longstringwithmorethan30characters{}FSDA@@#$()@#$_)1-longstringwithmorethan30characters{}FSDA@@#$("
-            ")@#$_)1- "
-        )
-        variables = {"user": self.user2}
-        executed = self.make_request(self.make_query(first_name=super_long_string), variables)
-        assert not executed["success"]
-        self.assertTrue(executed["errors"]["firstName"])
-        self.user2.refresh_from_db()
-        self.assertEqual(self.user2.first_name, "bar")
-
-    @mark.settings_b
-    def test_update_account_list_on_settings(self):
-        variables = {"user": self.user2}
-        executed = self.make_request(self.make_query(), variables)
-        assert executed["success"]
-        assert not executed["errors"]
-        self.user2.refresh_from_db()
-        self.assertEqual(self.user2.first_name, "firstname")
-
-    @mark.settings_b
-    def test_update_account_non_field_errors(self):
-        """
-        on settings b: first and last name are unique together,
-        so we can test the non field error for the error type
-        """
-        # first update a user
-        # NOTE this is deprecated as I didn't saw any need for that
-        # because there can easily be two persons with the same first and last name
-
-
-class UpdateAccountTestCase(UpdateAccountTestCaseMixin, ArgTestCase):
-    def make_query(self, first_name="firstname"):
+    def _arg_query(self, first_name="firstname"):
         return """
         mutation {
             updateAccount(firstName: "%s")
@@ -168,17 +16,7 @@ class UpdateAccountTestCase(UpdateAccountTestCaseMixin, ArgTestCase):
             first_name
         )
 
-    def get_unique_together_test_query(self):
-        return """
-        mutation {
-            updateAccount(firstName: "first", lastName: "last")
-                { success, errors  }
-        }
-        """
-
-
-class UpdateAccountRelayTestCase(UpdateAccountTestCaseMixin, RelayTestCase):
-    def make_query(self, first_name="firstname"):
+    def _relay_query(self, first_name="firstname"):
         return """
         mutation {
             updateAccount(input:{ firstName: "%s" })
@@ -188,10 +26,58 @@ class UpdateAccountRelayTestCase(UpdateAccountTestCaseMixin, RelayTestCase):
             first_name
         )
 
-    def get_unique_together_test_query(self):
-        return """
-        mutation {
-            updateAccount(input: {firstName: "first", lastName: "last"})
-                { success, errors  }
-        }
-        """
+    def test_update_account_unauthenticated(self, db_verified_user_status):
+        user_status = db_verified_user_status
+        user = user_status.user.obj
+        executed = self.make_request(self.make_query())
+        assert not executed["success"]
+        assert executed['errors']['nonFieldErrors'] == Messages.UNAUTHENTICATED
+
+    def test_update_account_not_verified(self, db_unverified_user_status, allow_login_not_verified):
+        user_status = db_unverified_user_status
+        user = user_status.user.obj
+        executed = self.make_request(self.make_query(), user_status=db_unverified_user_status)
+        assert not executed["success"]
+        assert executed["errors"]['nonFieldErrors'] == Messages.NOT_VERIFIED
+
+    def test_update_account(self, db_verified_user_status):
+        user_status = db_verified_user_status
+        user = user_status.user.obj
+        executed = self.make_request(self.make_query(), user_status=db_verified_user_status)
+        assert executed["success"]
+        assert not executed["errors"]
+        user.refresh_from_db()
+        assert user.first_name == "firstname"
+
+    def test_invalid_form(self, db_verified_user_status):
+        user_status = db_verified_user_status
+        user_obj = user_status.user.obj
+        super_long_string = (
+            "10" * 150  # django.auth first_name field is 150 characters or fewer.
+        )
+        executed = self.make_request(self.make_query(first_name=super_long_string),
+                                     user_status=db_verified_user_status
+                                     )
+        assert not executed["success"]
+        assert executed["errors"]["firstName"]
+        user_obj.refresh_from_db()
+        assert user_obj.first_name == user_status.user.username
+
+    # @mark.settings_b
+    def test_update_account_list_on_settings(self, db_verified_user_status):
+        user_status = db_verified_user_status
+        user_obj = user_status.user.obj
+        executed = self.make_request(self.make_query(), user_status=db_verified_user_status)
+        assert executed["success"]
+        assert not executed["errors"]
+        user_obj.refresh_from_db()
+        user_obj.first_name == "firstname"
+
+
+
+class TestArgUpdateAccount(UpdateAccountTestCaseMixin, ArgTestCase):
+    ...
+
+
+class TestRelayUpdateAccount(UpdateAccountTestCaseMixin, RelayTestCase):
+    ...
