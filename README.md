467f6e1a93acf114064db695a932ba12e0a29179
Nir <88795475+nrbnlulu@users.noreply.github.com>
2022-07-18 17:28:02 +0300

    migrate to async - Fixed async test that work: captcha, archive account, test_delete_account.py, test_login.py, test_password_change.py, test_password_reset.py, test_password_set.py, test_query.py, test_refresh_token.py, test_register.py, test_remove_secondary_email.py, test_revoke_token.py, test_send_password_reset_email.py

--- README.md
+++ README.md
@@ -13,14 +13,12 @@
 ### This Library is the strawberry version of ![Django-graphql-auth](https://github.com/pedrobern/django-graphql-auth/).
 
 Abstract all the basic logic of handling user accounts out of your app,
-so you don't need to think about it and can **get up and running faster**.
+so you don't need to think about it and can **get you up and running faster**.
 
 No lock-in. When you are ready to implement your own code or this package
 is not up to your expectations , it's *easy to extend or switch to
 your implementation*.
 
-
-## Documentation
 
 ### Docs can be found [here](https://nrbnlulu.github.io/strawberry-django-auth/)
 
@@ -81,63 +79,6 @@ class AuthMutation:
 
 schema = strawberry.Schema(mutation=AuthMutation)
 ```
-
-
-## User relay queries
-
-### Currently not supported
-expect to find it here when strawberry will natively support relay
-
-
-## Example
-
-Handling user accounts becomes super easy.
-
-```python
-mutation {
-  register(
-    email: "new_user@email.com",
-    username: "new_user",
-    password1: "123456super",
-    password2: "123456super",
-  ) {
-    success,
-    errors,
-    token,
-    refreshToken
-  }
-}
-```
-
-Check the status of the new user:
-
-```python
-u = UserModel.objects.last()
-u.status.verified
-# False
-```
-
-During the registration, an email with a verification link was sent.
-
-```python
-mutation {
-  verifyAccount(
-    token:"<TOKEN ON EMAIL LINK>",
-  ) {
-    success,
-    errors
-  }
-}
-```
-
-Now user is verified.
-
-```python
-u.status.verified
-# True
-```
-
-
 
 ## Contributing
 
--- tests/test_send_password_reset_email.py
+++ tests/test_send_password_reset_email.py
@@ -3,42 +3,63 @@ from unittest import mock
 
 from gqlauth.constants import Messages
 
-from .testCases import ArgTestCase, RelayTestCase
+from .testCases import ArgTestCase, RelayTestCase, UserType, AsyncArgTestCase, AsyncRelayTestCase
 
 
 class SendPasswordResetEmailTestCaseMixin:
-    def setUp(self):
-        unverified_user = self.register_user(email="foo@email.com", username="foo", verified=False)
-        self.user2 = self.register_user(
-            email="bar@email.com",
-            username="bar",
-            verified=True,
-            secondary_email="secondary@email.com",
+    def _arg_query(self, user: UserType):
+        return """
+        mutation {
+        sendPasswordResetEmail(email: "%s")
+            { success, errors }
+        }
+        """ % (
+            user.email
         )
 
-    def test_send_email_invalid_email(self):
+    def _relay_query(self, user: UserType):
+        return """
+        mutation {
+        sendPasswordResetEmail(input:{ email: "%s"})
+            { success, errors  }
+        }
+        """ % (
+            user.email
+        )
+
+    def test_send_email_invalid_email(self, db_verified_user_status):
         """
-        invalid email should be successful request
+        invalid email should be successful request.
+        (due to security measures)
         """
-        query = self.make_query("invalid@email.com")
+        user = db_verified_user_status.user
+        user.email = "invalid@email.com"
+        query = self.make_query(user)
         executed = self.make_request(query=query)
         assert executed["success"]
         assert not executed["errors"]
 
-    def test_invalid_form(self):
-        query = self.make_query("baremail.com")
+    def test_invalid_form(self, db_verified_user_status):
+        user = db_verified_user_status.user
+        user.email = "invalid * form@email.com"
+        query = self.make_query(user)
         executed = self.make_request(query=query)
         assert not executed["success"]
         assert executed["errors"]["email"]
 
-    def test_send_email_valid_email_verified_user(self):
-        query = self.make_query("bar@email.com")
+    def test_send_email_valid_email_verified_user(self, db_verified_user_status):
+        query = self.make_query(db_verified_user_status.user)
         executed = self.make_request(query=query)
         assert executed["success"]
         assert not executed["errors"]
 
-    def test_send_to_secondary_email(self):
-        query = self.make_query("secondary@email.com")
+    def test_send_to_secondary_email(self, db_verified_user_status):
+        user = db_verified_user_status.user
+        user.email = "foo@bar.com"
+        user.obj.status.secondary_email = user.email
+        user.obj.status.save()
+
+        query = self.make_query(user)
         executed = self.make_request(query=query)
         assert executed["success"]
         assert not executed["errors"]
@@ -47,33 +68,23 @@ class SendPasswordResetEmailTestCaseMixin:
         "gqlauth.models.UserStatus.send_password_reset_email",
         mock.MagicMock(side_effect=SMTPException),
     )
-    def test_send_email_fail_to_send_email(self):
-        mock
-        query = self.make_query("bar@email.com")
+    def test_send_email_fail_to_send_email(self, db_verified_user_status):
+        query = self.make_query(db_verified_user_status.user)
         executed = self.make_request(query=query)
         assert not executed["success"]
         assert executed["errors"]["nonFieldErrors"] == Messages.EMAIL_FAIL
 
 
-class SendPasswordResetEmailTestCase(SendPasswordResetEmailTestCaseMixin, ArgTestCase):
-    def make_query(self, email):
-        return """
-        mutation {
-        sendPasswordResetEmail(email: "%s")
-            { success, errors }
-        }
-        """ % (
-            email
-        )
+class TestArgSendPasswordResetEmail(SendPasswordResetEmailTestCaseMixin, ArgTestCase):
+    ...
 
 
-class SendPasswordResetEmailRelayTestCase(SendPasswordResetEmailTestCaseMixin, RelayTestCase):
-    def make_query(self, email):
-        return """
-        mutation {
-        sendPasswordResetEmail(input:{ email: "%s"})
-            { success, errors  }
-        }
-        """ % (
-            email
-        )
+class TestRelaySendPasswordResetEmail(SendPasswordResetEmailTestCaseMixin, RelayTestCase):
+    ...
+
+class TestAsyncArgSendPasswordResetEmail(SendPasswordResetEmailTestCaseMixin, AsyncArgTestCase):
+    ...
+
+
+class TestAsyncRelaySendPasswordResetEmail(SendPasswordResetEmailTestCaseMixin, AsyncRelayTestCase):
+    ...
