96866ac8bff715a1236c976ee0897bbf5c2e7926
ניר <88795475+nrbnlulu@users.noreply.github.com>
2024-04-06 22:21:56 +0300

    Update file paths in release.py and utils.py

--- .github/workflows/management/utils.py
+++ .github/workflows/management/utils.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 class PATHS:
     PROJECT_ROOT = Path(__file__).parent.parent.parent.parent
-    ROOT_CMAKE = PROJECT_ROOT / "CMakeLists.txt"
+    PROJECT_SOURCE = PROJECT_ROOT / "gqlauth"
     PYPROJECT_TOML = PROJECT_ROOT / "pyproject.toml"
     CHANGELOG = PROJECT_ROOT / "CHANGELOG.md"
     RELEASE_FILE = PROJECT_ROOT / "RELEASE.md"
