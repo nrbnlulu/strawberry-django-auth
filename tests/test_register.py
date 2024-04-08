67f6740bacdba3ce091e7e4b860d3adcdbff7f00
Hyun S. Moon <dr.moon.kr@gmail.com>
2024-04-08 23:01:00 +0900

    Refactor test code of register mutation in channels context

--- tests/test_register.py
+++ tests/test_register.py
@@ -50,14 +50,11 @@ def test_register_invalid_password_validation(verified_user_status_type, anonymo
     assert executed["errors"]
 
 
-async def test_channels_register(verified_user_status_type, async_client, captcha):
+async def test_channels_register(verified_user_status_type, captcha, verified_channels_app_communicator):
     us = verified_user_status_type
-    res = await async_client.post(
-        path="/arg_schema",
-        data={"query": _arg_query(us.user, captcha)},
-        content_type="application/json",
-    )
-    assert res.json()["data"]["register"]["success"] is True
+    query = _arg_query(us.user, captcha)
+    async for res in verified_channels_app_communicator.subscribe(query):
+        assert res.data["register"]["success"] is True
 
 
 def test_register_twice_fails(verified_user_status_type, anonymous_schema, db):
