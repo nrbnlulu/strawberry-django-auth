0634e1b2c49b0e3ab4432d0c491ec07b861d12a5
Hyun S. Moon <dr.moon.kr@gmail.com>
2024-04-24 04:16:24 +0900

    Add test code for DB migration

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,8 @@
 import pytest
+from django.apps import apps
+from django.test import TestCase
+from django.db.migrations.executor import MigrationExecutor
+from django.db import connection
 from django.test import override_settings
 from gqlauth.captcha.models import Captcha
 from gqlauth.core.constants import Messages
@@ -75,6 +79,52 @@ def login_query(request):
 @pytest.fixture()
 def archived_schema(db_archived_user_status, rf) -> SchemaHelper:
     return SchemaHelper.create(rf=rf, us_type=db_archived_user_status)
+
+
+class TestMigrations(TestCase):
+
+    @property
+    def app(self):
+        return apps.get_containing_app_config("gqlauth").name
+
+    migrate_from = None
+    migrate_to = None
+
+    def setUp(self):
+        assert self.migrate_from and self.migrate_to, \
+            "TestCase '{}' must define migrate_from and migrate_to     properties".format(type(self).__name__)
+        self.migrate_from = [(self.app, self.migrate_from)]
+        self.migrate_to = [(self.app, self.migrate_to)]
+        executor = MigrationExecutor(connection)
+        old_apps = executor.loader.project_state(self.migrate_from).apps
+
+        # Reverse to the original migration
+        executor.migrate(self.migrate_from)
+
+        self.setUpBeforeMigration(old_apps)
+
+        # Run the migration to test
+        executor = MigrationExecutor(connection)
+        executor.loader.build_graph()  # reload.
+        executor.migrate(self.migrate_to)
+
+        self.apps = executor.loader.project_state(self.migrate_to).apps
+
+    def setUpBeforeMigration(self, apps):
+        pass
+
+
+class TagsTestCase(TestMigrations):
+
+    migrate_from = '0001_initial'
+    migrate_to = '0002_alter_userstatus_options'
+
+    def setUpBeforeMigration(self, apps):
+        UserModel.objects.create(username='testusername')
+
+    def test_tags_migrated(self):
+        user = UserModel.objects.get(username='testusername')
+        self.assertTrue(user.status is not None)
 
 
 def default_test(res: ExecutionResult):
